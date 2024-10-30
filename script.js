6d30233e0c2f14b951b12d4b06e6bdc5e2b2e3bd
Troe <20bekarys04@gmail.com>
2024-10-30 15:28:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+// let weight = 45; // Number
+
+// let modelName = "Huawei";
+// let name ="Askhat"; // String (Мәтін)
+
+// let isAutumn = true;
+// let isNight = false; // Boolean
+
+// console.log(weight >50); //Boolean
+// const trashBin = null; //Null (іші бос)
+
+// const ufo = undefined; //Анықталмаған
+
+// const bag = ["book","pencil","apple"]
+
+
+
+
+
+
+
+
+// let score =0;
+
+// let isGoal = false; //тағайындау
+
+// //салыстыру/тексеру
+// if ( isGoal===true){
+//     score =1;
+// }
+// console.log(score)
+
+
+
+
+
+
+
+
+// let height = 145;
+// let bolady = null;
+
+// if (height >150) {
+//     bolady =true;
+
+//     console.log(bolady);
+// }
+// console.log(bolady);
+
+
+
+
+
+
+// let essay ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ex nam dignissimos aut, voluptate dolore ducimus beatae! Dolores odit nobis provident aliquid possimus, quisquam amet! Sed eum in iusto id."
+
+// let children = ["Aibaris","Nurislam","Islam"] //Array
+
+// let car = {
+//     name:'BMW',
+//     color: 'Black',
+//     year:1995,
+//     weight:'800kg'
+// } //Object,Объект 
+// console.log(car)
+
+
+
+
+
+let name = "Bekarys";
+let surname = "Nurbolat";
+let school = "#12"
+let weight = 65
+
+console.log("My name is", name)
+console.log("My surname is", surname)
+const birthday = "20.04.2009"
+console.log("My school at", school)
+console.log("My weight", weight)
